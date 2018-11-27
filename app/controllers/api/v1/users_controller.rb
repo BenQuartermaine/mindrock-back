17ba128d34ff48c8675dac56f977ea85b8e7b8ef
Benjamin Chen <yeehenchen@gmail.com>
2018-11-27 18:04:04 +0800

    added comments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def show
     @user = User.find(params[:id])
+    # create assignments, current challenge from frontend
   end
 
   def update
