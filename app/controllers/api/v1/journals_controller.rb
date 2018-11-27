7fc6cc81e591064febdd591ed9ba79b893528f48
Benjamin Chen <yeehenchen@gmail.com>
2018-11-27 18:49:02 +0800

    tested successfully, modified a bit the assignment_id params

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::JournalsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
+
   def create
+    @assignment = Assignment.find(params[:assignment_id])
+    # update assignment status
+    @assignment.status = true
+    @assignment.save
+
     @journal = Journal.new(journal_params)
+    @journal.assignment = @assignment
+
     if @journal.save
       render json: {
         journal: @journal
@@ -12,7 +21,7 @@ class Api::V1::JournalsController < Api::V1::BaseController
 
   private
   def journal_params
-    params.require(:journal).permit(:assignment_id, :content, :summary_tag_list, :photo_tag_list)
+    params.require(:journal).permit(:content, :summary_tag_list, :photo_tag_list)
   end
 
   def render_error
