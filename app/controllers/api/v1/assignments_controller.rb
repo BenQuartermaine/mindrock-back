dab8064702480609648f494b41e1a026f55916a4
AveryJin <avery.jin@gmail.com>
2018-11-27 14:13:01 +0800

    added journalscontroller#create

--- app/controllers/api/v1/assignments_controller.rb
+++ app/controllers/api/v1/assignments_controller.rb
@@ -1,4 +1,5 @@
-class AssignmentsController < ApplicationController
+class Api::V1::AssignmentsController < Api::V1::BaseController
+
   def show
   end
 end
--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::JournalsController < Api::V1::BaseController
   def create
+    @journal = Journal.new(journal_params)
+    if @journal.save
+      render json: {
+        journal: @journal
+      }
+    else
+      render_error
+    end
   end
 
+  private
+  def journal_params
+    params.require(:journal).permit(:assignment_id, :content, :summary_tag_list, :photo_tag_list)
+  end
+
+  def render_error
+    render json: {errors: @journal.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
