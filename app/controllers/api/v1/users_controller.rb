1ae7a6a634ac56a6f84b69810441968bec0e3a56
Benjamin Chen <yeehenchen@gmail.com>
2018-11-27 15:29:28 +0800

    added update controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,5 +4,22 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show, status: :updated
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatarUrl, :nickName)
+  end
+
+  def render_error
+    render json: { error: @user.errors.full_messages },
+           status: :unprocessable_entity
   end
 end
