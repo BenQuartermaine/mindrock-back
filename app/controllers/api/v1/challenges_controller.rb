46f043a53ee3f0618e5bff512d1693ad3286addf
Benjamin Chen <yeehenchen@gmail.com>
2018-11-27 18:27:49 +0800

    done, extract name & description & tag_list

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ChallengesController < Api::V1::BaseController
-
   def index
     @challenges = Challenge.all
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
   end
 end
