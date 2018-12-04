b53cfc3029910e086ce0b100c49f36ad8fadfa0b
AveryJin <avery.jin@gmail.com>
2018-12-04 18:04:04 +0800

    longest streak

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,25 @@ class User < ApplicationRecord
 
   validates :open_id, presence: true, uniqueness: true
 
+
+  def max
+    h = assignments.sort_by{|x| x.id}.map{|x| x.status }
+    arr = []
+    h.each_with_index do |x, i|
+      if x
+        arr << {index: i, status: x}
+      end
+    end
+    p arr
+    brr = []
+
+    arr.each_with_index do |x, i|
+      if i < arr.length - 1
+        brr << (arr[i+1][:index] - x[:index])
+      end
+    end
+    p brr
+    result = brr.map{|x| x== 1 ? 1 : '-'}.join().split('-').map{|x| x.length}.sort.last
+    result.nil? ? 0 : result + 1
+  end
 end
