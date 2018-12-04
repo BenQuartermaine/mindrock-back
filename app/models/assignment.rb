363eafd52781b53d25c4c54fb68b4354d7bdbdc3
Benjamin Chen <yeehenchen@gmail.com>
2018-12-04 12:43:45 +0800

    view of team

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,4 +4,23 @@ class Assignment < ApplicationRecord
   has_many :journals
 
   validates :date, presence: true
+
+  def journal_hash
+    u = User.find(self.user_id)
+    j = self.journals.first
+
+    if j.nil?
+      {
+        avatarUrl: u.avatarUrl,
+        content: "",
+        status: self.status
+      }
+    else
+      {
+        avatarUrl: u.avatarUrl,
+        content: j.content,
+        status: self.status
+      }
+    end
+  end
 end
